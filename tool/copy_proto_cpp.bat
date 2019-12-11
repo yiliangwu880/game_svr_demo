fb53e598a389dfb310baab8a6045ab75906ad3a3
yiliangwu880 <30390012@qq.com>
2019-12-11 16:15:41 +0800

    完善cmake的 proto

--- tool/copy_proto_cpp.bat
+++ tool/copy_proto_cpp.bat
@@ -1,7 +1,7 @@
 REM 复制远程linux proto导出文件到win目录
 
-xcopy Z:\test\game_svr_demo\proto\*.h ..\proto\ /sy
-xcopy Z:\test\game_svr_demo\proto\*.cc E:\my\game_svr_demo\proto\ /sy
+xcopy Z:\test\game_svr_demo\proto_cpp\*.h ..\proto_cpp\ /sy
+xcopy Z:\test\game_svr_demo\proto_cpp\*.cc E:\my\game_svr_demo\proto_cpp\ /sy
 REM xcopy Z:\test\game_svr_demo\dbproxy_svr\cpp_cfg\*.h E:\my\game_svr_demo\dbproxy_svr\cpp_cfg\ /sy
 
 pause
